7547db89f4eece65ad555cb7e2011d5677ab578f
Habeebullah Lawal <81315552+damnations@users.noreply.github.com>
2022-11-17 13:08:57 +0200

    prevent duplicate raspi-blinka.py scripts

--- setup.sh
+++ setup.sh
@@ -10,6 +10,10 @@ git clone https://github.com/UedaTakeyuki/handlers
 ln -s handlers/value/sender/send2monitor/send2monitor.py
 sudo sed -i "s/^enable_uart=.*/enable_uart=1/" /boot/config.txt
 cd ~
+FILE=~/raspi-blinka.py
+if [[ -f "$FILE" ]]; then
+    rm raspi-blinka.py
+fi
 sudo pip3 install --upgrade adafruit-python-shell
 wget https://raw.githubusercontent.com/adafruit/Raspberry-Pi-Installer-Scripts/master/raspi-blinka.py
 sudo python3 raspi-blinka.py
