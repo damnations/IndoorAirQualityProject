48cd57ad732fcb9375c7df31920905db2eefd98d
Habeebullah Lawal <81315552+damnations@users.noreply.github.com>
2022-11-30 19:25:20 +0200

    changes inside adafruit blinka script

--- setup.sh
+++ setup.sh
@@ -1,19 +1,23 @@
-cd ~
+#!/bin/bash
+cd $HOME 
 sudo apt update
 sudo apt upgrade -y
-sudo apt install -y python3-pip git
-sudo pip3 install --upgrade setuptools
+sudo apt autoremove -y
 git clone https://github.com/UedaTakeyuki/mh-z19.git
 cd mh-z19
 sudo pip3 install mh_z19 pondslider incremental_counter error_counter
 git clone https://github.com/UedaTakeyuki/handlers
 ln -s handlers/value/sender/send2monitor/send2monitor.py
 sudo sed -i "s/^enable_uart=.*/enable_uart=1/" /boot/config.txt
-cd ~
-FILE=~/raspi-blinka.py
+cd $HOME
+FILE=$HOME/raspi-blinka.py
 if [[ -f "$FILE" ]]; then
     rm raspi-blinka.py
 fi
 sudo pip3 install --upgrade adafruit-python-shell
 wget https://raw.githubusercontent.com/adafruit/Raspberry-Pi-Installer-Scripts/master/raspi-blinka.py
+sudo raspi-config nonint do_blanking 1
+sudo sed -i "s/do_serial 0/do_serial 2/
+             s/sudo apt-get install -y python3-pip/sudo apt install -y python3-pip git chromium-browser unclutter
+             s/apt-get/apt/g" $HOME/raspi-blinka.py
 sudo python3 raspi-blinka.py
