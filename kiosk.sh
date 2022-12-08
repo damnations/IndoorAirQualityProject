dc9da0cc62bac5e2582a9e05142b11943e268d64
Habeebullah Lawal <81315552+damnations@users.noreply.github.com>
2022-12-08 13:49:10 +0200

    added kiosk.sh

---
+++ kiosk.sh
@@ -0,0 +1,6 @@
+@xset s off
+@xset -dpms
+@xset s noblank
+
+unclutter &
+@chromium-browser --kiosk --incognito --noerrdialogs --disable-infobars file:/home/akhrs/IndoorAirQualityProject/Html/Display.html
