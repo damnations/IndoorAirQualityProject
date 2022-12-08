8d828a88aa9b182f6ba613441767a486769d5b43
Habeebullah Lawal <81315552+damnations@users.noreply.github.com>
2022-12-08 15:10:34 +0200

    window size flag

--- kiosk.sh
+++ kiosk.sh
@@ -3,4 +3,4 @@
 @xset s noblank
 
 unclutter &
-@chromium-browser --kiosk --incognito --noerrdialogs --disable-infobars file:/home/akhrs/IndoorAirQualityProject/Html/Display.html
+@chromium-browser --window-size=320,240 --kiosk --incognito --noerrdialogs --disable-infobars file:/home/akhrs/IndoorAirQualityProject/Html/Display.html
