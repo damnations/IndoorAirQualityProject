9dc7ca4803fa24ca1d52f13834db15d5feee5a54
Habeebullah Lawal <81315552+damnations@users.noreply.github.com>
2022-12-13 19:36:27 +0200

    launch chromium

--- kiosk.sh
+++ kiosk.sh
@@ -1,6 +1,5 @@
-@xset s off
-@xset -dpms
-@xset s noblank
-
-@unclutter &
-@chromium-browser --window-size=320,240 --kiosk --incognito --noerrdialogs --disable-infobars file:/home/akhrs/IndoorAirQualityProject/Html/Display.html
+xset s off
+xset -dpms
+xset s noblank
+unclutter &
+chromium-browser file:$HOME/IndoorAirQualityProject/Html/Display.html --new-window --window-size=320,240 --kiosk --incognito --noerrdialogs --disable-infobars 
