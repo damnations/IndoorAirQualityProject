56a9be53fe5e0ac5687c5af4884c93684584e165
Habeebullah Lawal <81315552+damnations@users.noreply.github.com>
2022-12-13 13:52:13 +0200

    update kiosk.sh

--- kiosk.sh
+++ kiosk.sh
@@ -2,5 +2,5 @@
 @xset -dpms
 @xset s noblank
 
-unclutter &
+@unclutter &
 @chromium-browser --window-size=320,240 --kiosk --incognito --noerrdialogs --disable-infobars file:/home/akhrs/IndoorAirQualityProject/Html/Display.html
